3b4f9a9cb3e5610296b9c0780bd791fb431ad4c7
Sam Thorogood <sam.thorogood@gmail.com>
2017-01-21 19:07:35 +1100

    helper to alt file

--- fusebase/fs.go
+++ fusebase/fs.go
@@ -7,8 +7,6 @@ import (
 
 	"log"
 	"os"
-	"strconv"
-	"strings"
 
 	"golang.org/x/net/context"
 )
@@ -104,22 +102,4 @@ func (node *fsNode) Write(ctx context.Context, req *fuse.WriteRequest, resp *fus
 
 	resp.Size = len(req.Data)
 	return nil
-}
-
-// bytesToValue converts the written bytes to a native type to write to Firebase, incorporating
-// the node's previous value (if any).
-func bytesToValue(b []byte, prev interface{}) interface{} {
-	if _, ok := prev.(string); ok {
-		return string(b) // previously a string, assume string
-	}
-
-	s := string(b)
-	f, err := strconv.ParseFloat(strings.TrimSpace(s), 64) // need to TrimSpace for newline
-	if err != nil {
-		return s // string (nb. echo generates newlines, most people don't want them)
-	}
-	if _, ok := prev.(bool); ok {
-		return f != 0 // prev was bool, assume number means bool again
-	}
-	return f // number
 }
---
+++ fusebase/value.go
@@ -0,0 +1,24 @@
+package fusebase
+
+import (
+	"strconv"
+	"strings"
+)
+
+// bytesToValue converts the written bytes to a native type to write to Firebase, incorporating
+// the node's previous value (if any).
+func bytesToValue(b []byte, prev interface{}) interface{} {
+	if _, ok := prev.(string); ok {
+		return string(b) // previously a string, assume string
+	}
+
+	s := string(b)
+	f, err := strconv.ParseFloat(strings.TrimSpace(s), 64) // need to TrimSpace for newline
+	if err != nil {
+		return s // string (nb. echo generates newlines, most people don't want them)
+	}
+	if _, ok := prev.(bool); ok {
+		return f != 0 // prev was bool, assume number means bool again
+	}
+	return f // number
+}
